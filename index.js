67e13ff2fc3adb1f07da21129c038cc0a140fe4f
Jonathan Svenheden <jonathan@svenheden.com>
2016-12-08 21:03:42 +0100

    Add formatting of regular swedish phone numbers

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const isSwedishMobileNumber = str => /^[070|072|073|076|079]{3}\d{7}$/.test(str);
+/**
+ * The specification:
+ *
+ * https://www.pts.se/sv/Bransch/Telefoni/Nummerfragor/Telefoninummerplanen/Telefoninummerplanens-disposition/
+ * http://www.sprakbruk.fi/media/sprakbruk_2009.2_artikel_14.pdf
+ */
+
+const subscriberNumberFormats = {
+    5: /^(\d{3})(\d{2})$/,
+    6: /^(\d{2})(\d{2})(\d{2})$/,
+    7: /^(\d{3})(\d{2})(\d{2})$/,
+    8: /^(\d{3})(\d{3})(\d{2})$/
+};
+
+const isSwedishMobilePhoneNumber = str => /^07[02369]{1}\d{7}$/.test(str);
+const isSwedishPhoneNumber = str => str[0] == 0 && str.length > 6 && str.length < 13;
 const normalizeSwedishPhoneNumber = str => str.replace(/[^\d\+]/g, '').replace(/^\+46/, '0');
+const hasTwoLetterAreaCode = str => /^08/.test(str);
+const hasThreeLetterAreaCode = str => /^0(11|13|16|18|19|21|23|26|31|33|35|36|40|42|44|46|54|60|63|90)/.test(str);
 const formatSwedishMobilePhoneNumber = str => str.replace(/^(\d{3})(\d{3})(\d{2})(\d{2})$/, '$1-$2 $3 $4');
+const formatSubscriberNumber = str => str.replace(subscriberNumberFormats[str.length], (str.length === 5 ? '$1 $2' : '$1 $2 $3'));
+
+const formatSwedishPhoneNumber = str => {
+    const [areaCode, subscriberNumber] = splitSwedishPhoneNumber(str);
+    return areaCode + '-' + formatSubscriberNumber(subscriberNumber);
+};
+
+const splitSwedishPhoneNumber = str => {
+    const pos = hasTwoLetterAreaCode(str) ? 2 : (hasThreeLetterAreaCode(str) ? 3 : 4);
+    return [str.substring(0, pos), str.substring(pos)];
+};
 
 const format = phoneNumber => {
   if (phoneNumber) {
     const normalized = normalizeSwedishPhoneNumber(phoneNumber);
-    return isSwedishMobileNumber(normalized) ? formatSwedishMobilePhoneNumber(normalized) : phoneNumber;
+
+    if (isSwedishMobilePhoneNumber(normalized)) {
+      return formatSwedishMobilePhoneNumber(normalized);
+    }
+
+    if (isSwedishPhoneNumber(normalized)) {
+      return formatSwedishPhoneNumber(normalized);
+    }
+
+    return phoneNumber;
   }
 };
 
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const format = require('./index');
 
-test('mobile phone numbers', function(assert) {
+test('mobile phone numbers', assert => {
   assert.equal(format('0707354909'), '070-735 49 09');
   assert.equal(format('070 73 549 09'), '070-735 49 09');
   assert.equal(format('070-7354-909'), '070-735 49 09');
@@ -9,5 +9,29 @@ test('mobile phone numbers', function(assert) {
   assert.equal(format('+46707354909'), '070-735 49 09');
   assert.equal(format('+45707354909'), '+45707354909');
   assert.equal(format('070735445663909'), '070735445663909');
+  assert.end();
+});
+
+test('phone numbers with 2 area code digits', assert => {
+  assert.equal(format('0812345'), '08-123 45');
+  assert.equal(format('08123456'), '08-12 34 56');
+  assert.equal(format('081234567'), '08-123 45 67');
+  assert.equal(format('0812345678'), '08-123 456 78');
+  assert.end();
+});
+
+test('phone numbers with 3 area code digits', assert => {
+  assert.equal(format('03112345'), '031-123 45');
+  assert.equal(format('031123456'), '031-12 34 56');
+  assert.equal(format('0311234567'), '031-123 45 67');
+  assert.equal(format('03112345678'), '031-123 456 78');
+  assert.end();
+});
+
+test('phone numbers with 4 area code digits', assert => {
+  assert.equal(format('030312345'), '0303-123 45');
+  assert.equal(format('0303123456'), '0303-12 34 56');
+  assert.equal(format('03031234567'), '0303-123 45 67');
+  assert.equal(format('030312345678'), '0303-123 456 78');
   assert.end();
 });
